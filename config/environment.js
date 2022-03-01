e0c57dd590b0f01d8d5ccee8f93e2b4d783284d9
Esin Yilmazbilek <86679064+EsinYilmazbilek@users.noreply.github.com>
2022-03-01 14:55:13 +0000

    Deleted environment file
    Until I fix the exposed MongoDB URI

--- config/environment.js
+++
@@ -1,3 +0,0 @@
-export const port = 4000
-export const dbURI = 'mongodb+srv://esinyilmazbilek:Greenjasmine_3@cluster0.yxh5u.mongodb.net/brands-db?retryWrites=true&w=majority'
-export const secret = 'its a secret'
